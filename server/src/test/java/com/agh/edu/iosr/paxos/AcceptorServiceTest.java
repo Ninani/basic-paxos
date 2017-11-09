cefd369236d1f081c5671b9171b74fc1739a2dd5
Anna Szczepanska <anna.szczepanska.krk@gmail.com>
2017-11-09 05:46:57 +0100

    More tests for Acceptor: accept phase

--- server/src/test/java/com/agh/edu/iosr/paxos/AcceptorServiceTest.java
+++ server/src/test/java/com/agh/edu/iosr/paxos/AcceptorServiceTest.java
@@ -18,58 +18,79 @@ public class AcceptorServiceTest {
     AcceptorService acceptorServiceSeqOne;
     PrepareRequest prepareRequestSeqZero;
     PrepareRequest prepareRequestSeqHigh;
-    AcceptRequest acceptRequestSeqZero;
-
+    
     @Before
     public void setUp() {
         acceptorServiceSeqZero = new AcceptorService(0, new AcceptedProposal(0, "val"));
         acceptorServiceSeqOne = new AcceptorService(1, new AcceptedProposal(1, "val"));
         prepareRequestSeqZero = new PrepareRequest(0);
         prepareRequestSeqHigh = new PrepareRequest(1000);
-        acceptRequestSeqZero = new AcceptRequest(0, "val");
     }
+
+//    Tests for PREPARE phase
 
     @Test
     public void returnsFalseIfSeqNumIsLower() {
         PrepareResponse prepareResponse = acceptorServiceSeqOne.prepare(prepareRequestSeqZero);
-        assertThat(prepareResponse.isAnswer()).isEqualTo(false);
+        assertThat(prepareResponse.getAnswer()).isEqualTo(false);
     }
 
     @Test
     public void returnsTrueIfSeqNumIsHigher() {
         PrepareResponse prepareResponse = acceptorServiceSeqZero.prepare(prepareRequestSeqHigh);
-        assertThat(prepareResponse.isAnswer()).isEqualTo(true);
+        assertThat(prepareResponse.getAnswer()).isEqualTo(true);
     }
 
-////    why it returns true?
+//    why it returns true?
 //    @Test
 //    public void returnsFalseIfSeqNumIsEqual() {
 //        PrepareResponse prepareResponse = acceptorServiceSeqZero.prepare(prepareRequestSeqZero);
-//        assertThat(prepareResponse.isAnswer()).isEqualTo(false);
-//    }
-
-////    it returns previous seqNum  - is it ok?
-//    @Test
-//    public void returnsSeqNumForAcceptedProposal() {
-//        PrepareResponse prepareResponse = acceptorServiceSeqZero.prepare(prepareRequestSeqHigh);
-//        assertThat(prepareResponse.isAnswer()).isEqualTo(true);
-//        long prepareRequestSeqNum = prepareRequestSeqHigh.getSequenceNumber();
-//        assertThat(prepareResponse.getAcceptedProposal().getSequenceNumber()).isEqualTo(prepareRequestSeqNum);
+//        assertThat(prepareResponse.getAnswer()).isEqualTo(false);
 //    }
 
     @Test
-    public void returnsValueForAcceptedProposal() {
+    public void returnsValueForAcceptedPrepareRequest() {
         PrepareResponse prepareResponse = acceptorServiceSeqZero.prepare(prepareRequestSeqHigh);
-        assertThat(prepareResponse.isAnswer()).isEqualTo(true);
+        assertThat(prepareResponse.getAnswer()).isEqualTo(true);
         assertThat(prepareResponse.getAcceptedProposal().getValue()).isEqualToIgnoringCase("val");
     }
 
-    // TODO: 11/8/17 write test cases for tests shown in YT presentation
+//    Tests for ACCEPT phase
 
     @Test
-    public void acceptRequestOnlyIfNumberIsEqualToSeqNumber() {
+    public void alwaysAcceptFirstAcceptRequest() {
+        long newSeqNumber = 1;
+        AcceptResponse acceptResponse = acceptorServiceSeqZero.accept(new AcceptRequest(newSeqNumber, "val"));
+        assertThat(acceptResponse.getSequenceNumber()).isEqualTo(newSeqNumber);
 
     }
 
+    @Test
+    public void acceptRequestIfNumberIsHigherThanPrevious() {
+        long newSeqNumber = 1;
+        AcceptResponse acceptResponse = acceptorServiceSeqZero.accept(new AcceptRequest(newSeqNumber, "val"));
+        assertThat(acceptResponse.getSequenceNumber()).isEqualTo(newSeqNumber);
+    }
+
+    @Test
+    public void acceptRequestIfNumberIsEqualToPrevious() {
+        long newSeqNumber = 0;
+        AcceptResponse acceptResponse = acceptorServiceSeqZero.accept(new AcceptRequest(newSeqNumber, "val"));
+        assertThat(acceptResponse.getSequenceNumber()).isEqualTo(newSeqNumber);
+    }
+
+//    Tests for two phases
+
+    @Test
+    public void returnsPrevoiuslyAcceptedNumberAndValueForNextPrepareRequest() {
+        long seqNumber = 6;
+        String value = "val";
+        AcceptorService acceptorService = new AcceptorService(seqNumber, new AcceptedProposal());
+        AcceptResponse acceptResponse = acceptorService.accept(new AcceptRequest(seqNumber, value));
+        // TODO: 11/9/17 check if request has been accepted
+        PrepareResponse prepareResponse = acceptorService.prepare(new PrepareRequest(8));
+        assertThat(prepareResponse.getAcceptedProposal().getSequenceNumber()).isEqualTo(seqNumber);
+        assertThat(prepareResponse.getAcceptedProposal().getValue()).isEqualToIgnoringCase(value);
+    }
 
 }
